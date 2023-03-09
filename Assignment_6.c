95e90e45a164ee3c7af0a6bf562715404a436082
Raman Thakur <thakuraman.online@gmail.com>
2023-03-09 09:42:15 +0530

    Q2,Q3 solve

--- Assignment_6.c
+++ Assignment_6.c
@@ -1,4 +1,4 @@
-//1. Write a program to calculate sum of first N natural numbers
+/*1. Write a program to calculate sum of first N natural numbers
 #include<stdio.h>
 
 int main(){
@@ -11,3 +11,35 @@ int main(){
     }
     
 }
+
+2. Write a program to calculate sum of first N even natural numbers
+*/
+#include<stdio.h>
+
+int main(){
+
+    printf("Sum of how many natural numbers do you want ?\n");
+
+    int count=0,sum=0;
+    scanf("%d",& count);
+
+    for (int i = 1; i < count+1; i++)
+    {
+        sum=sum+i;
+    }
+
+    printf("\nThe sum of first %d natural no is %d",count,sum);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
